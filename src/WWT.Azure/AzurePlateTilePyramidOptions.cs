beed48d17a365c5313d01c293148e22e7a2eb848
Taylor Southwick <twsouthwick@outlook.com>
2020-09-23 13:28:50 -0700

    Reorganize source files and remove unused files

---
+++ src/WWT.Azure/AzurePlateTilePyramidOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace WWT.Azure
+{
+    public class AzurePlateTilePyramidOptions
+    {
+        public string StorageUri { get; set; }
+
+        public bool CreateContainer { get; set; }
+
+        public bool OverwriteExisting { get; set; }
+    }
+}
