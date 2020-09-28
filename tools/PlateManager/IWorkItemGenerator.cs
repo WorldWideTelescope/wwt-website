ed2fad67e70a25b818b466691b564ce78c979796
Taylor Southwick <twsouthwick@outlook.com>
2020-09-28 14:59:03 -0700

    Convert PlateUploader to more generic PlateManager cli
    This removes the dependency on WinForms, and provides a single tool that can be used to manage plate file tasks. A pub/sub model is used via the System.Threading.Channels namespace. This allows for a pipeline infrastructure to be used to process the files into images and then to upload them.

---
+++ tools/PlateManager/IWorkItemGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlateManager
+{
+    internal interface IWorkItemGenerator
+    {
+        IEnumerable<Func<int, int, Task>> GenerateWorkItems(string plateFile, string baseUrl, CancellationToken token);
+    }
+}
---
+++ tools/PlateManager/Program.cs
@@ -0,0 +1,85 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WWT.Azure;
+
+namespace PlateManager
+{
+    class Program
+    {
+        static Task<int> Main(string[] args)
+        {
+            var command = new Command("upload")
+            {
+                new Option<IEnumerable<FileInfo>>(new[] { "--file", "-f" }) { IsRequired = true }.ExistingOnly(),
+                new Option<Uri>("--storage", () => new Uri("https://127.0.0.1:10000/devstoreaccount1/")),
+                new Option<bool>("--interactive"),
+                new Option<string>("--baseUrl", ()=>"baseUrl")
+            };
+
+            command.Handler = CommandHandler.Create<IEnumerable<FileInfo>, Uri, bool, string>(Run);
+
+            var root = new RootCommand
+            {
+                command
+            };
+
+            return root.InvokeAsync(args);
+        }
+
+        static async Task Run(IEnumerable<FileInfo> file, Uri storage, bool interactive, string baseUrl)
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole();
+            });
+
+            services.AddTransient<IWorkItemGenerator, PlateFileWorkItemGenerator>();
+
+            services.AddSingleton<AzurePlateTilePyramid>();
+            services.AddSingleton<Processor>();
+
+            services.AddSingleton(new AzurePlateTilePyramidOptions
+            {
+                CreateContainer = true,
+                OverwriteExisting = true
+            });
+
+            services.AddAzureClients(builder =>
+            {
+                builder.AddBlobServiceClient(storage);
+
+                if (interactive)
+                {
+                    builder.UseCredential(new InteractiveBrowserCredential());
+                }
+                else
+                {
+                    builder.UseCredential(new DefaultAzureCredential());
+                }
+            });
+
+            var options = new ProcessorOptions
+            {
+                BaseUrl = baseUrl,
+                Files = file.Select(p => p.FullName)
+            };
+
+            using var container = services.BuildServiceProvider();
+
+            await container.GetRequiredService<Processor>().ProcessAsync(options, default);
+        }
+    }
+}
