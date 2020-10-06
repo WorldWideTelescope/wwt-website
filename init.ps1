0f4ffb3c9403f187feb5d65066e8d459fcca6dda
Peter Williams <peter@newton.cx>
2020-10-06 11:34:20 -0400

    init.ps1: bubble up failures

--- init.ps1
+++ init.ps1
@@ -3,11 +3,14 @@ param ([switch]$restore)
 if ($restore) {
   Push-Location src/WWTMVC5
   npm install
+  if (-not $?) { throw "npm install failed with error code $LastExitCode" }
   Pop-Location
 }
 
 Push-Location src/WWTMVC5
 bower install
-grunt copy
+if (-not $?) { throw "bower install failed with error code $LastExitCode" }
 
+grunt copy
+if (-not $?) { throw "grunt copy failed with error code $LastExitCode" }
 Pop-Location
