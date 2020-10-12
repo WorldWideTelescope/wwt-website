b8741fab0d350751180a681cefe4d61bff63fe05
Taylor Southwick <twsouthwick@outlook.com>
2020-10-12 16:01:08 -0700

    Use abstraction in Jupiter endpoint

---
+++ tests/WWT.Providers.Tests/JupiterProviderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using WWTWebservices;
+using Xunit;
+
+namespace WWT.Providers.Tests
+{
+    public class JupiterProviderTests : ProviderTests<JupiterProvider>
+    {
+        protected override int MaxLevel => 7;
+
+        protected override Action<IResponse> NullStreamResponseHandler => null;
+
+        protected override Action<IResponse> StreamExceptionResponseHandler => null;
+
+        protected override void ExpectedResponseAboveMaxLevel(IResponse response)
+        {
+            Assert.Empty(response.OutputStream.ToArray());
+        }
+
+        protected override Stream GetStreamFromPlateTilePyramid(IPlateTilePyramid plateTiles, int level, int x, int y)
+            => plateTiles.GetStream(Options.WwtTilesDir, "Jupiter.plate", level, x, y);
+    }
+}
