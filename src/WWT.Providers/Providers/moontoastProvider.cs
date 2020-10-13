3797929238ab44211e7297b417d3b6e950e2ff80
Taylor Southwick <twsouthwick@outlook.com>
2020-10-12 17:21:03 -0700

    Use IPlateTilePyramid in moon* providers

--- src/WWT.Providers/Providers/moontoastProvider.cs
+++ src/WWT.Providers/Providers/moontoastProvider.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Configuration;
 using System.IO;
 using WWTWebservices;
 
@@ -7,18 +6,24 @@ namespace WWT.Providers
 {
     public class moontoastProvider : RequestProvider
     {
+        private readonly IPlateTilePyramid _plateTiles;
+        private readonly FilePathOptions _options;
+
+        public moontoastProvider(IPlateTilePyramid plateTiles, FilePathOptions options)
+        {
+            _plateTiles = plateTiles;
+            _options = options;
+        }
+
         public override void Run(IWwtContext context)
         {
-            string wwtTilesDir = ConfigurationManager.AppSettings["WWTTilesDir"] + "\\LROWAC";
+            string wwtTilesDir = Path.Combine(_options.WwtTilesDir, "LROWAC");
 
             string query = context.Request.Params["Q"];
             string[] values = query.Split(',');
             int level = Convert.ToInt32(values[0]);
             int tileX = Convert.ToInt32(values[1]);
             int tileY = Convert.ToInt32(values[2]);
-
-            int octsetlevel = level;
-            string filename;
 
             if (level > 10)
             {
@@ -30,45 +35,34 @@ namespace WWT.Providers
             if (level < 7)
             {
                 context.Response.ContentType = "image/png";
-                Stream s = PlateTilePyramid.GetFileStream(wwtTilesDir + "\\L0X0Y0.plate", level, tileX, tileY);
-                int length = (int)s.Length;
-                byte[] data = new byte[length];
-                s.Read(data, 0, length);
-                context.Response.OutputStream.Write(data, 0, length);
-                context.Response.Flush();
-                context.Response.End();
-                return;
+
+                using (Stream s = _plateTiles.GetStream(wwtTilesDir, "L0X0Y0.plate", level, tileX, tileY))
+                {
+                    s.CopyTo(context.Response.OutputStream);
+                    context.Response.Flush();
+                    context.Response.End();
+                    return;
+                }
             }
             else
             {
-                int L = level;
-                int X = tileX;
-                int Y = tileY;
-                int powLev5Diff = (int)Math.Pow(2, L - 3);
-                int X32 = X / powLev5Diff;
-                int Y32 = Y / powLev5Diff;
-                filename = string.Format(wwtTilesDir + @"\L3x{0}y{1}.plate", X32, Y32);
+                int powLev5Diff = (int)Math.Pow(2, level - 3);
+                int X32 = tileX / powLev5Diff;
+                int Y32 = tileY / powLev5Diff;
 
-                int L5 = L - 3;
-                int X5 = X % powLev5Diff;
-                int Y5 = Y % powLev5Diff;
+                int L5 = level - 3;
+                int X5 = tileX % powLev5Diff;
+                int Y5 = tileY % powLev5Diff;
                 context.Response.ContentType = "image/png";
-                Stream s = PlateTilePyramid.GetFileStream(filename, L5, X5, Y5);
-                int length = (int)s.Length;
-                byte[] data = new byte[length];
-                s.Read(data, 0, length);
-                context.Response.OutputStream.Write(data, 0, length);
-                context.Response.Flush();
-                context.Response.End();
-                return;
 
+                using (Stream s = _plateTiles.GetStream(wwtTilesDir, $"L3x{X32}y{Y32}.plate", L5, X5, Y5))
+                {
+                    s.CopyTo(context.Response.OutputStream);
+                    context.Response.Flush();
+                    context.Response.End();
+                    return;
+                }
             }
-
-
-
-            // This file has returns which cause this warning to show in the generated files.
-            // This should be refactored, but that will be a bigger change.
-#pragma warning disable 0162
         }
     }
 }
---
+++ tests/WWT.Providers.Tests/MoontoastProviderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using WWTWebservices;
+using Xunit;
+
+namespace WWT.Providers.Tests
+{
+    public class MoontoastProviderTests : ProviderTests<moontoastProvider>
+    {
+        protected override int MaxLevel => 10;
+
+        protected override Action<IResponse> NullStreamResponseHandler => null;
+
+        protected override Action<IResponse> StreamExceptionResponseHandler => null;
+
+        protected override void ExpectedResponseAboveMaxLevel(IResponse response)
+        {
+            Assert.Empty(response.ContentType);
+            Assert.Empty(response.OutputStream.ToArray());
+        }
+
+        protected override Stream GetStreamFromPlateTilePyramid(IPlateTilePyramid plateTiles, int level, int x, int y)
+        {
+            string prefix = Path.Combine(Options.WwtTilesDir, "LROWAC");
+
+            if (level < 7)
+            {
+                return plateTiles.GetStream(prefix, "L0X0Y0.plate", level, x, y);
+            }
+            else
+            {
+                int powLev5Diff = (int)Math.Pow(2, level - 3);
+                int X32 = x / powLev5Diff;
+                int Y32 = y / powLev5Diff;
+
+                int L5 = level - 3;
+                int X5 = x % powLev5Diff;
+                int Y5 = y % powLev5Diff;
+
+                return plateTiles.GetStream(prefix, $"L3x{X32}y{Y32}.plate", L5, X5, Y5);
+            }
+        }
+    }
+}
