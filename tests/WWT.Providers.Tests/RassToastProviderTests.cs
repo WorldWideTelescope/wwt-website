c5da26c0e4037c9ced35dc07a2cb5daa554fb4d9
Taylor Southwick <twsouthwick@outlook.com>
2020-10-13 09:48:06 -0700

    Update providers P-R with IPlateTilePyramid

---
+++ tests/WWT.Providers.Tests/RassToastProviderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using WWTWebservices;
+using Xunit;
+
+namespace WWT.Providers.Tests
+{
+    public class RassToastProviderTests:ProviderTests<RassToastProvider>
+    {
+        protected override int MaxLevel => 7;
+
+        protected override Action<IResponse> NullStreamResponseHandler => null;
+
+        protected override Action<IResponse> StreamExceptionResponseHandler => null;
+
+
+        protected override Stream GetStreamFromPlateTilePyramid(IPlateTilePyramid plateTiles, int level, int x, int y)
+            => plateTiles.GetStream(Options.WwtTilesDir, "rass.plate", level, x, y);
+
+        protected override void ExpectedResponseAboveMaxLevel(IResponse response)
+        {
+            Assert.Empty(response.ContentType);
+            Assert.Empty(response.OutputStream.ToArray());
+        }
+    }
+}
