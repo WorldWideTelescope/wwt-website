748175f43c9628aa54f409acb79674b6e1bcba0f
Taylor Southwick <twsouthwick@outlook.com>
2020-10-19 14:23:14 -0700

    Add caching to IPlateTilePyramid
    This change adds an in-memory and redis cache layer.  This is configurable via web.config to turn caching on or off. If a redis connection string is available, it will cache there; otherwise, it will use an in-memory implementation.

---
+++ src/WWT.PlateFiles.Caching/CachedPlateTilePyramid.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using WWTWebservices;
+
+namespace WWT.PlateFiles.Caching
+{
+    public abstract class CachedPlateTilePyramid : IPlateTilePyramid
+    {
+        public CachedPlateTilePyramid(IPlateTilePyramid other)
+        {
+            InnerPyramid = other;
+        }
+
+        public IPlateTilePyramid InnerPyramid { get; }
+
+        public IAsyncEnumerable<string> GetPlateNames(CancellationToken token)
+            => InnerPyramid.GetPlateNames(token);
+
+        public Stream GetStream(string pathPrefix, string plateName, int level, int x, int y)
+        {
+            var context = new TileContext(InnerPyramid)
+            {
+                PathPrefix = pathPrefix,
+                PlateName = plateName,
+                Level = level,
+                X = x,
+                Y = y
+            };
+
+            return new MemoryStream(GetOrUpdateCache(context));
+        }
+
+        public Stream GetStream(string pathPrefix, string plateName, int tag, int level, int x, int y)
+        {
+            var context = new TileContext(InnerPyramid)
+            {
+                PathPrefix = pathPrefix,
+                PlateName = plateName,
+                Tag = tag,
+                Level = level,
+                X = x,
+                Y = y
+            };
+
+            return new MemoryStream(GetOrUpdateCache(context));
+        }
+
+        protected abstract byte[] GetOrUpdateCache(TileContext context);
+
+        public class TileContext
+        {
+            private readonly IPlateTilePyramid _other;
+
+            public TileContext(IPlateTilePyramid other)
+            {
+                _other = other;
+            }
+
+            public string GetKey()
+            {
+                var sb = new StringBuilder(PathPrefix.Length + PlateName.Length + 20);
+
+                sb.Append(PathPrefix);
+                sb.Append("_");
+                sb.Append(PlateName);
+                sb.Append("_");
+
+                if (Tag.HasValue)
+                {
+                    sb.Append(Tag.Value);
+                    sb.Append("_");
+                }
+
+                sb.Append("L");
+                sb.Append(Level.ToString());
+
+                sb.Append("X");
+                sb.Append(X.ToString());
+
+                sb.Append("Y");
+                sb.Append(Y.ToString());
+
+                return sb.ToString();
+            }
+
+            public string PathPrefix { get; set; }
+
+            public string PlateName { get; set; }
+
+            public int? Tag { get; set; }
+
+            public int Level { get; set; }
+
+            public int X { get; set; }
+
+            public int Y { get; set; }
+
+            public byte[] GetResult()
+            {
+                var result = Tag.HasValue
+                    ? _other.GetStream(PathPrefix, PlateName, Tag.Value, Level, X, Y)
+                    : _other.GetStream(PathPrefix, PlateName, Level, X, Y);
+
+                using (var ms = new MemoryStream())
+                {
+                    result.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
