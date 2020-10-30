d0fad60fa6243cd10affa7da598d7445cc42db2d
Chris Roberts <chrisrt@microsoft.com>
2020-10-29 18:29:47 -0700

    wired up redis for session offloading

---
+++ src/WWTMVC5/RedisConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace WWTMVC5
+{
+    /// <summary>
+    /// Delegates the configuration of the Redis Cache to use another app setting
+    /// This centralizes our redis connection string and allows use in session offloading
+    /// </summary>
+    public class RedisConfig
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.AppSettings["RedisConnectionString"];
+        }
+    }
+}
