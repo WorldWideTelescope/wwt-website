d14de04b9688496ffea1b92f0c3a9a276ba89b7d
Chris Roberts <chrisrt@microsoft.com>
2020-10-30 08:21:35 -0700

    Offloaded session to Redis (#164)
    * modified catalog and catalog2 to use blob storage
    
    * Propogated cancellation token
    
    * wired up redis for session offloading
    
    * Added commentary about how the RedisConfig class is used

---
+++ src/WWTMVC5/RedisConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace WWTMVC5
+{
+    /// <summary>
+    /// Delegates the configuration of the Redis Cache to use another app setting
+    /// This centralizes our redis connection string and allows use in session offloading
+    /// </summary>
+    public class RedisConfig
+    {
+        /// <summary>
+        /// This method is called by the Microsoft Redis Session provider, 
+        /// and is only referenced by name in the Web.Config sessionState block,
+        /// do not remove as long as it is referenced therein
+        /// </summary>
+        /// <returns>The shared Redis connection string</returns>
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.AppSettings["RedisConnectionString"];
+        }
+    }
+}
