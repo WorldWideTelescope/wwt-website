3f6ea2f990c0e93c255d09aa76ce26b15ec499de
Taylor Southwick <twsouthwick@outlook.com>
2020-11-03 11:39:09 -0800

    Revert to old martiantile.aspx with updates (#178)
    This brings back the logic from the previous martiantile.aspx, but
    updates it to use the Azure access patterns.
    
    However, it accesses a platefile via exploded png values, so a layer was
    added to the plate tile pyramid accees level to know how to handle that.
    I think in the long term, we may want to change this to more of a
    pipeline where a chain of IPlateTilePyramid implementations can identify
    which platefiles they know how to handle; for now, inheriting is solving
    the problem.

---
+++ src/WWT.Azure/PlateFiles/MarsMolaAwareSeekableAzurePlateTilePyramid.cs
@@ -0,0 +1,51 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WWT.Azure
+{
+    public class MarsMolaAwareSeekableAzurePlateTilePyramid : MarsAwareSeekableAzurePlateTilePyramid
+    {
+        private readonly BlobContainerClient _container;
+
+        public MarsMolaAwareSeekableAzurePlateTilePyramid(
+            AzurePlateTilePyramidOptions options,
+            AzureServiceAccessor services,
+            ILogger<SeekableAzurePlateTilePyramid> logger)
+            : base(options, services, logger)
+        {
+            _container = services.WwtFiles.GetBlobContainerClient("marsmola");
+        }
+
+        public override Task<Stream> GetStreamAsync(string pathPrefix, string plateName, int level, int x, int y, CancellationToken token)
+        {
+            if (string.Equals(plateName, "marsmola.plate", StringComparison.Ordinal))
+            {
+                return GetMarsMolaStream(level, x, y, token);
+            }
+            else
+            {
+                return base.GetStreamAsync(pathPrefix, plateName, level, x, y, token);
+            }
+        }
+
+        private async Task<Stream> GetMarsMolaStream(int level, int x, int y, CancellationToken token)
+        {
+            var blob = _container.GetBlobClient($"marsmolaL{level}X{x}Y{y}.png");
+
+            try
+            {
+                return await blob.OpenReadAsync(_readOptions, token).ConfigureAwait(false);
+            }
+            catch (RequestFailedException e)
+            {
+                _logger.LogError(e, "Error getting MarsMola stream");
+                return null;
+            }
+        }
+    }
+}
