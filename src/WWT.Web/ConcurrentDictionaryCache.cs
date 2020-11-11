6c442e7cb2495baef0c314b237a5a2260a7a666e
Taylor Southwick <twsouthwick@outlook.com>
2020-11-11 09:30:47 -0800

    Add .NET 5 project head
    This adds initial support for ASP.NET Core running on .NET 5. Thanks to
    the previous refactoring work, there is very little that needed to be
    done to support this.
    
    With this change, the wwturl tests all pass against .NET 5. There may be
    some issues not captured by this, but looks good for an initial push.

---
+++ src/WWT.Web/ConcurrentDictionaryCache.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using WWT.Providers;
+
+namespace WWT.Web
+{
+    public class ConcurrentDictionaryCache : ICache
+    {
+        private readonly ConcurrentDictionary<string, object> _cache;
+
+        public ConcurrentDictionaryCache()
+        {
+            _cache = new ConcurrentDictionary<string, object>();
+        }
+
+        public object this[string key]
+        {
+            get
+            {
+                if (_cache.TryGetValue(key, out var value))
+                {
+                    return value;
+                }
+
+                return null;
+            }
+            set => _cache.AddOrUpdate(key, value, (s, o) => o);
+        }
+
+        public object Add(string key, object value, DateTime absoluteExpiration, TimeSpan slidingExpiration)
+            => this[key] = value;
+
+        public object Get(string key) => this[key];
+
+        public void Remove(string key) => _cache.TryRemove(key, out _);
+    }
+
+}
---
+++ src/WWT.Web/Startup.cs
@@ -0,0 +1,146 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using System;
+using WWT.Azure;
+using WWT.Providers;
+using WWT.Tours;
+using WWTWebservices;
+
+namespace WWT.Web
+{
+    public partial class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
+            services.AddApplicationInsightsTelemetry(options =>
+            {
+                options.InstrumentationKey = _configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+            });
+
+            services.AddRequestProviders(options =>
+            {
+
+                options.WwtToursDBConnectionString = _configuration["WWTToursDBConnectionString"];
+            });
+
+            services
+             .AddAzureServices(options =>
+             {
+                 options.StorageAccount = _configuration["AzurePlateFileStorageAccount"];
+                 options.MarsStorageAccount = _configuration["MarsStorageAccount"];
+             })
+             .AddPlateFiles(options =>
+             {
+                 _configuration.Bind(options);
+                 options.Container = _configuration["PlateFileContainer"];
+             })
+             .AddThumbnails(options =>
+             {
+                 options.ContainerName = _configuration["ThumbnailContainer"];
+                 options.Default = _configuration["DefaultThumbnail"];
+             })
+             .AddCatalog(options =>
+             {
+                 options.ContainerName = _configuration["CatalogContainer"];
+             })
+             .AddTours(options =>
+             {
+                 options.ContainerName = _configuration["TourContainer"];
+             })
+             .AddTiles(options =>
+             {
+                 options.ContainerName = _configuration["ImagesTilerContainer"];
+             });
+
+            services
+                .AddCaching(options =>
+                {
+                    _configuration.Bind(options);
+                    options.RedisCacheConnectionString = _configuration["RedisConnectionString"];
+                    options.SlidingExpiration = TimeSpan.Parse(_configuration["SlidingExpiration"]);
+                })
+                .CacheType<IMandelbrot>(m => m.Add(nameof(IMandelbrot.CreateMandelbrot)))
+                .CacheType<IVirtualEarthDownloader>(plates => plates.Add(nameof(IVirtualEarthDownloader.DownloadVeTileAsync)))
+                .CacheType<IOctTileMapBuilder>(plates => plates.Add(nameof(IOctTileMapBuilder.GetOctTileAsync)))
+                .CacheType<IPlateTilePyramid>(plates => plates.Add(nameof(IPlateTilePyramid.GetStreamAsync)))
+                .CacheType<IThumbnailAccessor>(plates => plates
+                    .Add(nameof(IThumbnailAccessor.GetThumbnailStreamAsync))
+                    .Add(nameof(IThumbnailAccessor.GetDefaultThumbnailStreamAsync)))
+                .CacheType<ITourAccessor>(plates => plates
+                    .Add(nameof(ITourAccessor.GetAuthorThumbnailAsync))
+                    .Add(nameof(ITourAccessor.GetTourAsync))
+                    .Add(nameof(ITourAccessor.GetTourThumbnailAsync)));
+
+            services.AddLogging(builder =>
+            {
+                builder.AddFilter("Swick.Cache", LogLevel.Trace);
+                builder.AddDebug();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                MapWwtEndpoints(endpoints);
+            });
+        }
+
+        private static void MapWwtEndpoints(IEndpointRouteBuilder endpoints)
+        {
+            var cache = new ConcurrentDictionaryCache();
+            var endpointManager = endpoints.ServiceProvider.GetRequiredService<EndpointManager>();
+
+            var @public = new CacheControlHeaderValue { Public = true };
+            var nocache = new CacheControlHeaderValue { NoCache = true };
+
+            foreach (var (endpoint, providerType) in endpointManager)
+            {
+                endpoints.MapGet(endpoint, ctx =>
+                {
+                    var provider = (RequestProvider)ctx.RequestServices.GetRequiredService(providerType);
+
+                    ctx.Response.ContentType = provider.ContentType;
+                    ctx.Response.GetTypedHeaders().CacheControl = provider.IsCacheable ? @public : nocache;
+
+                    return provider.RunAsync(new AspNetCoreWwtContext(ctx, cache), ctx.RequestAborted);
+                });
+            }
+        }
+    }
+}
