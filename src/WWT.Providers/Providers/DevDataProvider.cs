f68a94a2dd383986877ec8e9bd04e5aadd60e9b4
Taylor Southwick <twsouthwick@outlook.com>
2020-11-12 08:37:18 -0800

    Respond to feedback

--- src/WWT.Providers/Providers/DevDataProvider.cs
+++ src/WWT.Providers/Providers/DevDataProvider.cs
@@ -1,18 +1,21 @@
-﻿using System.Threading;
+﻿using Microsoft.Extensions.Logging;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WWT.Providers
 {
-    [RequestEndpoint("/wwtweb/data/dev")]
+    [RequestEndpoint("/v2/data/dev_export")]
     public class DevDataProvider : RequestProvider
     {
         private const int MaxLevel = 4;
 
         private readonly IDevDataAccessor _devData;
+        private readonly ILogger<DevDataProvider> _logger;
 
-        public DevDataProvider(IDevDataAccessor devData)
+        public DevDataProvider(IDevDataAccessor devData, ILogger<DevDataProvider> logger)
         {
             _devData = devData;
+            _logger = logger;
         }
 
         public override string ContentType => ContentTypes.Zip;
@@ -26,8 +29,11 @@ namespace WWT.Providers
 
             if (level > MaxLevel)
             {
+                _logger.LogInformation("Level {Level} was requested above max {MaxLevel}", level, MaxLevel);
                 level = MaxLevel;
             }
+
+            _logger.LogInformation("Retrieving dev data for datasets up to {Level}", level);
 
             using var result = await _devData.GetDevDataAsync(level, token);
 
