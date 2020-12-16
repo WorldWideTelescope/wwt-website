dc08759e68632fb97cf492f3610066ee7200b5a6
Peter Williams <peter@newton.cx>
2020-12-15 22:49:12 -0500

    Remove many now-unused configuration settings

--- src/WWT.Providers/WwtOptions.cs
+++ src/WWT.Providers/WwtOptions.cs
@@ -6,23 +6,11 @@ namespace WWT.Providers
     {
         public int TourVersionCheckIntervalMinutes { get; set; }
 
-        public bool LoginTracking { get; set; }
-
-        public string LoggingConn { get; set; } = string.Empty;
-
-        public string DataDir { get; set; } = string.Empty;
-
         public string DssTerapixelDir { get; set; } = string.Empty;
-
-        public string DSSTileCache { get; set; } = string.Empty;
-
-        public string DssToastPng { get; set; } = string.Empty;
 
         public string WWTDEMDir { get; set; } = string.Empty;
 
         public string WwtTilesDir { get; set; } = string.Empty;
-
-        public string WwtTourCache { get; set; } = string.Empty;
 
         public string WwtToursTourFileUNC { get; set; } = string.Empty;
 
