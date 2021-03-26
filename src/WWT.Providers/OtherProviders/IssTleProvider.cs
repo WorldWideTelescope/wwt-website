fa74b97b4824bd4eb378c13ab90a7b433c6db6f2
Peter Williams <peter@newton.cx>
2021-03-26 12:56:07 -0400

    WWT.Providers: merge IssTle.Aspx.cs into IssTleProvider.cs

--- src/WWT.Providers/OtherProviders/IssTleProvider.cs
+++ src/WWT.Providers/OtherProviders/IssTleProvider.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace WWT.Providers
 {
     [RequestEndpoint("/wwtweb/isstle.aspx")]
-    public class IsstleProvider : isstle
+    public class IsstleProvider : RequestProvider
     {
         public override string ContentType => ContentTypes.Text;
 
@@ -79,6 +79,54 @@ namespace WWT.Providers
 
                 await context.Response.WriteAsync(reply, token);
             }
+        }
+
+        static bool IsTLECheckSumGood(string line)
+        {
+            if (line.Length != 69)
+            {
+                return false;
+            }
+
+            int checksum = 0;
+            for (int i = 0; i < 68; i++)
+            {
+                switch (line[i])
+                {
+                    case '1':
+                        checksum += 1;
+                        break;
+                    case '2':
+                        checksum += 2;
+                        break;
+                    case '3':
+                        checksum += 3;
+                        break;
+                    case '4':
+                        checksum += 4;
+                        break;
+                    case '5':
+                        checksum += 5;
+                        break;
+                    case '6':
+                        checksum += 6;
+                        break;
+                    case '7':
+                        checksum += 7;
+                        break;
+                    case '8':
+                        checksum += 8;
+                        break;
+                    case '9':
+                        checksum += 9;
+                        break;
+                    case '-':
+                        checksum += 1;
+
+                        break;
+                }
+            }
+            return ('0' + (char)(checksum % 10)) == line[68];
         }
     }
 }
