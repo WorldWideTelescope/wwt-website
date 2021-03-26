3f3ba1efb5e487e61157feb092e7a85b0b46e01d
Peter Williams <peter@newton.cx>
2021-03-26 13:04:30 -0400

    WWT.Providers: update ISS TLE to use a new service
    The old NASA endpoint has been decommissioned.

--- src/WWT.Providers/OtherProviders/IssTleProvider.cs
+++ src/WWT.Providers/OtherProviders/IssTleProvider.cs
@@ -10,20 +10,21 @@ namespace WWT.Providers
     {
         public override string ContentType => ContentTypes.Text;
 
+        const string theUrl = "https://api.wheretheiss.at/v1/satellites/25544/tles?format=text";
+
         public override async Task RunAsync(IWwtContext context, CancellationToken token)
         {
-            string url = "";
-
-            url = "http://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html";
+            string reply = "";
 
             try
             {
-                string reply = (string)context.Cache["WWTISSTLE"];
+                reply = (string) context.Cache["WWTISSTLE"];
                 DateTime date = DateTime.Now;
                 TimeSpan ts = new TimeSpan(100, 0, 0, 0);
-                if (context.Cache["WWTISSTLDATE"] != null)
+
+                if (context.Cache["WWTISSTLEDATE"] != null)
                 {
-                    date = (DateTime)context.Cache["WWTISSTLDATE"];
+                    date = (DateTime) context.Cache["WWTISSTLEDATE"];
                     ts = DateTime.Now - date;
                 }
 
@@ -31,54 +32,49 @@ namespace WWT.Providers
                 {
                     using (WebClient wc = new WebClient())
                     {
-                        string data = wc.DownloadString(url);
-
+                        string data = wc.DownloadString(theUrl);
                         string[] lines = data.Split(new char[] { '\n', '\r' });
+
                         string line1 = "";
                         string line2 = "";
+
                         for (int i = 0; i < lines.Length; i++)
                         {
                             lines[i] = lines[i].Trim();
+
                             if (lines[i].Length == 69 && IsTLECheckSumGood(lines[i]))
                             {
                                 if (line1.Length == 0 && lines[i].Substring(0, 1) == "1")
                                 {
                                     line1 = lines[i];
                                 }
+
                                 if (line2.Length == 0 && lines[i].Substring(0, 1) == "2")
                                 {
                                     line2 = lines[i];
                                 }
                             }
                         }
+
                         if (line1 == "" || line2 == "")
                         {
-                            reply = "1 25544U 98067A   13274.85334491  .00007046  00000-0  12878-3 0  7167\n";
-                            reply += "2 25544  51.6486 299.7368 0003212  97.7461 254.0523 15.50562392851247\n";
-                            reply += "Cached during government shutdown";
-
-                        }
-                        else
-                        {
-
-                            reply = line1 + "\n" + line2 + "\nLaste Updated:" + DateTime.Now;
+                            throw new SystemException("TLE webservice response did not include valid TLE data");
                         }
 
+                        reply = line1 + "\n" + line2 + "\nLast Updated:" + DateTime.Now;
                         context.Cache["WWTISSTLE"] = reply;
-                        context.Cache["WWTISSTLDATE"] = DateTime.Now;
+                        context.Cache["WWTISSTLEDATE"] = DateTime.Now;
                     }
                 }
-
-                await context.Response.WriteAsync(reply, token);
             }
             catch (Exception)
             {
-                string reply = "1 25544U 98067A   13274.85334491  .00007046  00000-0  12878-3 0  7167\n";
+                reply = "1 25544U 98067A   13274.85334491  .00007046  00000-0  12878-3 0  7167\n";
                 reply += "2 25544  51.6486 299.7368 0003212  97.7461 254.0523 15.50562392851247\n";
-                reply += "Cached during NASA Downtime";
-
-                await context.Response.WriteAsync(reply, token);
+                reply += "WARNING: cached response due to backend failure";
             }
+
+            await context.Response.WriteAsync(reply, token);
         }
 
         static bool IsTLECheckSumGood(string line)
